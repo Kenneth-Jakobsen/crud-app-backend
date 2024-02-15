15f30b05093a4081719dd32882eae8a0c75be9da
ImPhame <jakobsenkenneth@hotmail.no>
2024-02-15 15:55:59 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API in port 3000");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:zv45uffug58MYtAj@backenddb.nfot2rw.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
---
+++ routes/product.route.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product.model.js");
+
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updatedProduct,
+  deleteProduct,
+} = require("../controllers/product.controllers.js");
+
+router.get("/", getProducts);
+
+router.get("/:id", getProduct);
+
+router.post("/", createProduct);
+
+router.put("/:id", updatedProduct);
+
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
